e23e3438ee351b656d9ba5d50358214e64db6a49
Oleksandrozhoh <oleksandrozhoh@gmail.com>
2025-06-03 16:38:25 -0500

    shared card component, merging templates with ng-content tag

--- src/app/task/task-item/task-item.component.ts
+++ src/app/task/task-item/task-item.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from './task.model'
+import { CardComponent } from "../../shared/card/card.component";
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
